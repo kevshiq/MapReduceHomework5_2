2b823a2406f3c57760a9aa8cbd7a445a74a9a785
kevshiq <3162105840@qq.com>
2024-10-24 19:21:59 +0800

    update

--- src/main/java/com/example/hadoophomework/SortCountReducer.java
+++ src/main/java/com/example/hadoophomework/SortCountReducer.java
@@ -7,13 +7,13 @@ import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
 public class SortCountReducer extends Reducer<LongWritable, Text, Text, LongWritable> {
-    long seq = 1;
+    private long seq = 1;
 
     @Override
     protected void reduce(LongWritable key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
         for (Text val : values) {
-            Text output = new Text(seq + "\t" + val.toString());
-            context.write(output, key);
+            Text outputKey = new Text(seq + "\t" + val.toString());
+            context.write(outputKey, key);
             seq += 1;
             if (seq >= 100) {
                 break;
